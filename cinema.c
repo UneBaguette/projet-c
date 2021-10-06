0623f3777acc37988934f06fd70282bb742ade80
unknown <thomasiger42@gmail.com>
2021-10-06 11:00:57 +0200

    first commit

---
+++ cinema.c
@@ -0,0 +1,26 @@
+#include stdio.h
+#include cs50.h
+#include string.h
+
+
+int main(void)
+{
+    string filmCategorieEnfant[3];
+    string filmCategorieAdo[3];
+    string filmCategorieAdulte[3];
+
+    string filmCategorieEnfant[0] = Marsupilami;
+    string filmCategorieEnfant[1] = Nemo;
+    string filmCategorieEnfant[2] = Harry Potter à l'école des sorciers
+
+    int ask = get_int(Quelle est ton âge  (en années)n);
+    if (ask = 11)
+    {
+        printf(on);
+    }
+    else
+    {
+        printf(nn);
+    }
+
+}
